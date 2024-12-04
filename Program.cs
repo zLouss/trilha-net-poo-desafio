6bbae79de2b0714aff2cebd3a42a667c1199c637
Luis Victor Belo <luis.victor.belo@gmail.com>
2024-12-03 21:04:32 -0300

    Desafio concluído

--- Program.cs
+++ Program.cs
@@ -1,3 +1,19 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Testando Nokia:");
+        Smartphone nokia = new Nokia("123456", "Nokia X", "111222333444", 64);
+        nokia.Ligar();
+        nokia.ReceberLigacao();
+        nokia.InstalarAplicativo("WhatsApp");
+
+        Console.WriteLine("\nTestando iPhone:");
+        Smartphone iphone = new Iphone("789101", "iPhone 13", "555666777888", 128);
+        iphone.Ligar();
+        iphone.ReceberLigacao();
+        iphone.InstalarAplicativo("Instagram");
+    }
+}
